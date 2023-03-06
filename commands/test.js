d2e9c60aba97ccc7b19d86655da048244c3df531
Luca Argentieri <Lucaargentieri-98@hotmail.it>
2023-03-06 09:28:51 +0100

    update test

--- commands/test.js
+++ commands/test.js
@@ -1,5 +1,7 @@
 const { SlashCommandBuilder } = require("discord.js");
 
+console.log("test");
+
 module.exports = {
   data: new SlashCommandBuilder().setName("test").setDescription("Replies with test!"),
   async execute(interaction) {
